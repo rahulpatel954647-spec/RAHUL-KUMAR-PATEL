746bd6fff7a0fd657a9d5e4b838c8f8ae8e3e164
Rahul kumar Patel <rahulpatel954647@gmail.com>
2026-06-15 22:06:45 -0700

    Add first.js with variable types and console logs
    Add first.js with variable types and console logs

---
+++ first.js
@@ -0,0 +1,38 @@
+// console.log("hi there");
+
+// alert("welcome");
+
+
+// let name = "Abc";
+// name = "cd";
+// console.log(name);
+
+const pi = 3.14;
+
+// var age = 18;
+
+
+let city = "Mohali"; //string
+let fruit = 'Mango'; //string
+
+console.log(typeof city);
+
+let marks = 65;  //number
+console.log(typeof marks); 
+
+let isLoggedin = true; //boolean
+console.log(typeof isLoggedin);
+
+let country; //undefined
+console.log(typeof country);  
+
+let school = null;  //null
+console.log(school); 
+
+let name = "your_name";
+let age = 20;
+
+console.log("my name is " + name);
+
+//String interpolation
+console.log(`my name is ${name}. My age is ${age}`);
