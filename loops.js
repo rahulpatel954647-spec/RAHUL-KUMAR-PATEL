db17b3f285ebcb1bba0a2e77fd8d51ba0fc55562
Rahul kumar Patel <rahulpatel954647@gmail.com>
2026-06-15 21:58:05 -0700

    Add various loop examples in loops.js
    loops.js

---
+++ loops.js
@@ -0,0 +1,35 @@
+let i = 6;  //entry controlled
+while (i<5){
+    console.log(i);
+    i++;
+}
+
+let x= 5;  //exit controlled
+do {
+    console.log(x);
+    x++;
+} while(x<5);
+
+
+for(let y = 1; y <=20; y++){
+    if(y % 2 === 0){
+        console.log(y);
+    }
+}
+
+for(let y = 1; y <=20; y++){
+    
+        console.log(y*y);
+    
+}
+
+for(let y = 20; y >=1; y--){
+    
+        console.log(y);
+    
+}
+
+
+
+
+
